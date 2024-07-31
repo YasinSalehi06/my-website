6c070e09361b1b2f786f24b458e9b9224a1c120d
Yasin Salehi <contactyasinsalehi@gmail.com>
2024-07-30 22:38:34 -0400

    update

---
+++ chat-example/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+const path = '/chatapp';
+
+app.use(express.static(__dirname));
+
+app.get(path, (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.of(path).on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', (msg) => {
+    io.of(path).emit('chat message', msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
